8fd87f3bc8cb3ce93bd9d63d6bca6b7eee8e24ea
Brent Mifsud <18543934+BrentMifsud@users.noreply.github.com>
2022-09-02 16:30:13 -0400

    update readme

--- Package.swift
+++ Package.swift
@@ -16,16 +16,19 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AsyncValue",
-            targets: ["AsyncValue"]),
+            targets: ["AsyncValue"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AsyncValue",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "AsyncValueTests",
-            dependencies: ["AsyncValue"]),
+            dependencies: ["AsyncValue"]
+        ),
     ]
 )
